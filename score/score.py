d1013fa5f108d817118edb9c68499231cecdfa13
Pierre BRESSY <pierre.bressy@heig-vd.ch>
2022-02-17 13:34:18 +0100

    fix #1

--- score/score.py
+++ score/score.py
@@ -18,7 +18,7 @@ class Score:
 
     @property
     def mark(self):
-        return max(1.0, min(6.0, round(self.got / self.total * 5. + 1.)))
+        return max(1.0, min(6.0, round(5. * self.got / self.total + 1., 1)))
 
     @property
     def points(self):
--- score/tests/test_score.py
+++ score/tests/test_score.py
@@ -9,8 +9,19 @@ class TestCriteria(TestCase):
     def test_file(self):
         with open(os.path.join(dir_path, 'criteria.yml')) as f:
             data = Score(f)
+        print(f'{data.points.got}/{data.points.total}')
         self.assertEqual(data.mark, 4.5)
         self.assertEqual(data.points.got, 7)
         self.assertEqual(data.points.total, 10)
         self.assertEqual(data.points.bonus, 2)
         self.assertTrue(data.success)
+
+    def test_neg_file(self):
+        with open(os.path.join(dir_path, 'criteria-neg.yml')) as f:
+            data = Score(f)
+        print(f'{data.points.got}/{data.points.total}')
+        self.assertEqual(data.mark, 1.0)
+        self.assertEqual(data.points.got, -8)
+        self.assertEqual(data.points.total, 10)
+        self.assertEqual(data.points.bonus, 0)
+        self.assertFalse(data.success)

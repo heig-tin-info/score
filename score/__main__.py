7b20fd2b8a18d2e1d262a568bde0a139b5eabd45
pierrebressy <pierre.bressy@heig-vd.ch>
2022-02-18 08:10:08 +0100

    Update __main__.py
    new option no-color

--- score/__main__.py
+++ score/__main__.py
@@ -7,13 +7,15 @@ from .score import Score
 @click.command()
 @click.argument('file', type=click.File('r'), default='criteria.yml')
 @click.option('--verbose', '-v', is_flag=True)
-def cli(file, verbose):
+@click.option('--nocolor', '-nc', is_flag=True)
+def cli(file, verbose,nocolor):
     score = Score(file)
     if verbose:
         print(f'Got {score.got:g} points + {score.bonus:g} points out of {score.total:g} points')
-
-    click.secho(f'{score.mark:g}', fg="green" if score.success else "red")
-
+    if nocolor:
+        print('%.1f', score.mark)
+    else:
+        click.secho(f'{score.mark:g}', fg="green" if score.success else "red")
 
 if __name__ == '__main__':
     cli()

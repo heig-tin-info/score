d58af15185c7a2ed52659ca8a3cf96767f200dba
pierrebressy <pierre.bressy@heig-vd.ch>
2022-02-18 08:15:47 +0100

    Update __main__.py
    fix format

--- score/__main__.py
+++ score/__main__.py
@@ -13,7 +13,7 @@ def cli(file, verbose,nocolor):
     if verbose:
         print(f'Got {score.got:g} points + {score.bonus:g} points out of {score.total:g} points')
     if nocolor:
-        print('%.1f', score.mark)
+        print('%.1f' %score.mark)
     else:
         click.secho(f'{score.mark:g}', fg="green" if score.success else "red")
 

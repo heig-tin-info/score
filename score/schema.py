a11186351973693d6641cf2285919cab4a257885
Yves Chevallier <yves.chevallier@heig-vd.ch>
2022-06-23 14:21:07 +0200

    Add $test on schema

--- score/schema.py
+++ score/schema.py
@@ -1,4 +1,5 @@
-from voluptuous import Optional, Schema, Required, All, Any, Self, Length
+from voluptuous import Invalid, Optional, Schema, Required, All, Any, Self, Length
+from ruamel.yaml import load, RoundTripLoader
 
 Section = Schema({
     str: Any({
@@ -11,3 +12,26 @@ Section = Schema({
 Criteria = Schema({
     'criteria': Section
 })
+
+class Validate:
+    def __init__(self, stream):
+        self._yaml = load(stream, Loader=RoundTripLoader)
+        return self.validate()
+
+    def validate(self):
+        try:
+            self.data = Criteria(self._yaml)
+        except Invalid as e:
+            path = '/'.join(e.path)
+            try: 
+                node = self._yaml
+                for key in e.path:
+                    if (hasattr(node[key], '_yaml_line_col')):
+                        node = node[key]
+                    else:
+                        break
+                print(f"Error: validation failed on line {node._yaml_line_col.line}:{node._yaml_line_col.col} (/{path}): {e.error_message}")
+            except:
+                print(e)
+        else:
+            return self.data
